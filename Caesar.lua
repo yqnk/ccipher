0be744c58c663e61b3e0b5acb5f73f2c66bba551
Yqnk <yan.bertile@gmail.com>
2022-10-14 00:38:13 +0200

    finished

---
+++ Caesar.lua
@@ -0,0 +1,24 @@
+local function shift(letter, s)
+    if string.upper(letter) == letter then
+        print("lmaj")
+        return string.char((string.byte(letter) + s - 65) % 26 + 65)
+    else
+        return string.char((string.byte(letter) + s - 97) % 26 + 97)
+    end
+end
+
+local function encode(w, s)
+    local result = ""
+    for i = 1, #w do
+        if string.sub(w, i, i) == " " then
+            result = result..(" ")
+        else
+            result = result..(shift(string.sub(w, i, i), s))
+        end
+    end
+    return result
+end
+
+local function decode(w, s)
+    return encode(w, -s)
+end
